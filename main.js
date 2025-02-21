56cc417c759e58523f696599e34487521462a3bc
Khlaf Hamza <91045072+KhlafHamza@users.noreply.github.com>
2025-02-21 14:46:39 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Navigation bar effects on scroll
+
+window.addEventListener("scroll", function(){
+    const header = this.document.querySelector("header");
+    header.classList.toggle("sticky", this.window.scrollY > 0);
+})
