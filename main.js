a3f58a9c781c5e42f8da6f4a1c97d339d19abf16
Khlaf Hamza <91045072+KhlafHamza@users.noreply.github.com>
2025-02-21 19:30:38 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -4,3 +4,36 @@ window.addEventListener("scroll", function(){
     const header = this.document.querySelector("header");
     header.classList.toggle("sticky", this.window.scrollY > 0);
 })
+
+
+
+
+//Service Section - Modal
+const serviceModals = document.querySelectorAll(".service-modal");
+const learnmoreBtns = document.querySelectorAll(".learn-more-btn");
+const modalCloseBtns = document.querySelectorAll(".learn-close-btn");
+
+
+
+
+var modal = function(modalClick){
+    serviceModals[modalClick].classList.add("active");
+
+}
+
+
+learnmoreBtns.forEach((learnmoreBtn, i) =>{
+    learnmoreBtn.addEventListener("click", () =>{
+        modal(i);
+    });
+});
+
+
+
+modalCloseBtns.forEach((modalCloseBtn)=>{
+    modalCloseBtn.addEventListener("click", () => {
+        serviceModals.forEach((modalView) => {
+            modalView.classList.remove("active");
+        });
+    });
+});
